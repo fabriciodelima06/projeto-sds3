cd6c5adfa255453cefdb34b10c5c80f2e08567ef
fabriciodelima06 <fabriciodelima06@gmail.com>
2021-05-09 23:59:31 -0300

    correção na conexão

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = 'https://sds3-fabricio.herokuapp.com' 
+
+//não leu a variavel de ambiente do netlify
+//process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
